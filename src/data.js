6422c065da752dc74cd7b4e4b20ea7fc302d38b1
DevUmair22 <108020563+DevUmair22@users.noreply.github.com>
2023-08-14 18:00:32 -0400

    first commit

---
+++ src/data.js
@@ -0,0 +1,9 @@
+import { faFacebook, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+const icons = [
+
+   <FontAwesomeIcon icon={faTwitter} size="sm" />,
+   <FontAwesomeIcon icon={faFacebook} size="sm" />, <FontAwesomeIcon icon={faInstagram} size="sm" />, <FontAwesomeIcon icon={faLinkedin} size="sm" />
+]
+
+export default icons
